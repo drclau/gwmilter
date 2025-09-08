57fdb65d61fee177c93dfb2ef37e5953acc61d4e
Claudiu Dragalina-Paraipan <dr.clau@gmail.com>
2025-09-08 19:54:18 +0300

    process global options from INI file (i.e. outside of sections)
    also: deleted useless comments

--- src/cfg2/ini_reader.cpp
+++ src/cfg2/ini_reader.cpp
@@ -7,16 +7,24 @@ namespace cfg2 {
 ConfigNode parseIniFile(const std::string &filename)
 {
     CSimpleIniA ini;
-
-    // Load the INI file
     SI_Error rc = ini.LoadFile(filename.c_str());
     if (rc != SI_OK) {
         throw std::runtime_error("Failed to parse INI file '" + filename + "' (error code: " + std::to_string(rc) +
                                  ")");
     }
 
-    // Create root config node
     ConfigNode root{"config", "", {}, NodeType::ROOT};
+
+    // Add global keys directly to root (keys outside of any section)
+    CSimpleIniA::TNamesDepend globalKeys;
+    ini.GetAllKeys("", globalKeys);
+    // sort global keys by loading order
+    globalKeys.sort([](const auto &a, const auto &b) { return a.nOrder < b.nOrder; });
+
+    for (const auto &key: globalKeys) {
+        const char *value = ini.GetValue("", key.pItem, "");
+        root.children.emplace_back(ConfigNode{key.pItem, value ? value : "", {}, NodeType::VALUE});
+    }
 
     // Get all sections
     CSimpleIniA::TNamesDepend sections;
@@ -25,24 +33,18 @@ ConfigNode parseIniFile(const std::string &filename)
     sections.sort([](const auto &a, const auto &b) { return a.nOrder < b.nOrder; });
 
     for (const auto &section: sections) {
-        // Create a section node
         ConfigNode sectionNode{section.pItem, "", {}, NodeType::SECTION};
 
-        // Get all keys for this section
         CSimpleIniA::TNamesDepend keys;
         ini.GetAllKeys(section.pItem, keys);
         // sort keys by the loading order. this is not strictly necessary, but it is consistent with sections sorting.
         keys.sort([](const auto &a, const auto &b) { return a.nOrder < b.nOrder; });
 
         for (const auto &key: keys) {
-            // Get the value for this key
             const char *value = ini.GetValue(section.pItem, key.pItem, "");
-
-            // Create key-value node and add to section
             sectionNode.children.emplace_back(ConfigNode{key.pItem, value ? value : "", {}, NodeType::VALUE});
         }
 
-        // Add section to root
         root.children.push_back(std::move(sectionNode));
     }
 
